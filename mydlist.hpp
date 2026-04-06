2c3dcafdefefa3274fcaa029c87696224d7b2e3d
Dongsun Kim <darkrsw@gmail.com>
2026-04-06 19:26:23 +0900

    Update mydlist.hpp

--- mydlist.hpp
+++ mydlist.hpp
@@ -28,43 +28,96 @@ private:
 public:
 	MyDoublyLinkedList()
 	{
+	    this->head = new MyNode<T>();
+	    this->tail = new MyNode<T>();
+	    this->head->setNext(tail);
+		this->head->setPrev(NULL);
+		this->tail->setPrev(head);
+		this->tail->setNext(NULL);
+		this->isize = 0;
 	}
 
 	~MyDoublyLinkedList()
 	{
+		// while( ! this->empty() )
+		// 	removeFromHead();
 	}
 
 	bool isEmpty()
 	{
+		return (this->isize == 0);
 	}
 
 	int getSize()
 	{
+		return this->isize;
 	}
 
 
 	MyNode<T>* getHeadNode()
 	{
+		if( this->head->getNext() != NULL )
+			return this->head->getNext();
+		else
+			return NULL;
 	}
 
 	MyNode<T>* getTailNode()
 	{
+		if( this->tail->getPrev() != NULL )
+			return this->tail->getPrev();
+		else
+			return NULL;
 	}
 
 	void addToHead(T* in)
 	{
+		MyNode<T>* newNode = new MyNode<T>();
+		newNode->set(in);
+		newNode->setNext(this->head->getNext());
+		this->head->getNext()->setPrev(newNode);
+		this->head->setNext(newNode);
+		newNode->setPrev(this->head);
+
+		this->isize += 1;
 	}
 
 	void removeFromHead()
 	{
+		MyNode<T>* temp = this->head->getNext();
+
+		this->head->setNext(this->head->getNext()->getNext());
+		this->head->getNext()->setPrev(this->head);
+
+		delete temp;
+
+		this->isize -= 1;
 	}
 
 
 	void addToTail(T* in)
 	{
+		MyNode<T>* newNode = new MyNode<T>();
+		newNode->set(in);
+		newNode->setNext(this->tail);
+		newNode->setPrev(this->tail->getPrev());
+		this->tail->getPrev()->setNext(newNode);
+		this->tail->setPrev(newNode);
+
+		this->isize += 1;
 	}
 
 	void removeFromTail()
 	{
+	    MyNode<T>* temp = this->tail->getPrev();
+
+		MyNode<T>* newPrev = this->tail->getPrev()->getPrev();
+
+		newPrev->setNext(this->tail);
+	    this->tail->setPrev(newPrev);
+
+	    delete temp;
+
+	    this->isize -= 1;
 	}
 };
